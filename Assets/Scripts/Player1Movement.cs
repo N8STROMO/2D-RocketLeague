bed9ced4534a6d5c27402740d0e71416b79fd8fb
NO8STROMO <johntpaul93@gmail.com>
2017-01-29 15:37:04 -0500

    Added a stop material to keep truck within bounds; implemented basic movement for truck

--- Assets/Scripts/Player1Movement.cs
+++ Assets/Scripts/Player1Movement.cs
@@ -4,13 +4,54 @@ using UnityEngine;
 
 public class Player1Movement : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
+    public Rigidbody2D rb2d;
+    public float speed;
+
+	/// <summary>
+    /// 
+    /// </summary>
+	void Start ()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
 	}
 	
-	// Update is called once per frame
-	void Update () {
-		
+	/// <summary>
+    /// 
+    /// </summary>
+	void Update ()
+    {
+        TruckHorizontalMovement();
 	}
+    
+    /// <summary>
+    /// 
+    /// </summary>
+    void TruckHorizontalMovement ()
+    {
+        bool movementRight = Input.GetKey(KeyCode.RightArrow);
+        bool movementLeft = Input.GetKey(KeyCode.LeftArrow);
+
+        if (movementRight)
+        {
+            rb2d.velocity = new Vector2(speed, 0);
+        }
+
+        else if (movementLeft)
+        {
+            rb2d.velocity = new Vector2(-speed, 0);
+        }
+
+        else
+        {
+            rb2d.velocity = Vector2.zero;
+        }
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    void TruckJump ()
+    {
+
+    }
 }
