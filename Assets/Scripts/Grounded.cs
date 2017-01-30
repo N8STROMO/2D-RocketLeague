6020e63e20140e884e86be70bd1eae220b71e346
NO8STROMO <johntpaul93@gmail.com>
2017-01-30 11:47:14 -0500

    Added jumping to the Truck; Trying to figure out how to ground the Trucks to enable gravity and perfect jumping

---
+++ Assets/Scripts/Grounded.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grounded : MonoBehaviour
+{
+    public Movement movement;
+
+    private void Start()
+    {
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        
+    }
+}
+
+
+  
+
---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+
+    public Rigidbody2D rb2d;
+    public float speed;
+    public float jumpHeight;
+
+	/// <summary>
+    /// 
+    /// </summary>
+	void Start ()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+	}
+	
+	/// <summary>
+    /// 
+    /// </summary>
+	void Update ()
+    {
+        TruckHorizontalMovement();
+        TruckJump();
+        FacingDirection();
+	}
+
+    /// <summary>
+    /// 
+    /// </summary>
+    void TruckHorizontalMovement ()
+    {
+        bool movementRight = Input.GetKey(KeyCode.RightArrow);
+        bool movementLeft = Input.GetKey(KeyCode.LeftArrow);
+
+        if (movementRight)
+        {
+            rb2d.velocity = new Vector2(speed, 0);
+        }
+
+        else if (movementLeft)
+        {
+            rb2d.velocity = new Vector2(-speed, 0);
+        }
+
+        else
+        {
+            rb2d.velocity = Vector2.zero;
+        }
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    void FacingDirection()
+    {
+        bool movementRight = Input.GetKey(KeyCode.RightArrow);
+        bool movementLeft = Input.GetKey(KeyCode.LeftArrow);
+
+        if (movementRight)
+        {
+            rb2d.transform.localScale = new Vector3(1, 1, 1);
+        }
+
+        if (movementLeft)
+        {
+            rb2d.transform.localScale = new Vector3(-1, 1, 1);
+        }
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    void TruckJump ()
+    {
+        bool jump = Input.GetKey(KeyCode.UpArrow);
+
+        if (jump)
+        {
+            rb2d.AddForce(Vector2.up * jumpHeight);
+        }
+
+    }
+}
